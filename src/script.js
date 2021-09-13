b55ee8174ad10d5e3caff79a0a1cea1c82400735
ekazebee <85964699+ekazebee@users.noreply.github.com>
2021-09-13 15:38:23 -0700

    display real forecast data using the api

--- src/script.js
+++ src/script.js
@@ -24,27 +24,41 @@ let showCurrentTime = document.querySelector("#current-time");
 let currentDate = new Date();
 showCurrentTime.innerHTML = formatDate(currentDate);
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let upcomingDay = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[upcomingDay];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
-  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
 
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="col">
-      <div class="weather-forecast-date">${day}</div>
-      <img
-        src="http://openweathermap.org/img/wn/50d@2x.png"
+  forecast.forEach(function (forecastDay, index) {
+    if ((index > 0) & (index < 6)) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col">
+      <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+      <img class="forecast-icons"
+        src="http://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png"
         alt=""
         width="42"
       />
       <div class="weather-forecast-temperatures">
-        <span class="weather-forecast-temperature-max">73</span> /
-        <span class="weather-forecast-temperature-min">57</span>
+        <span class="weather-forecast-temperature-max">${Math.round(
+          forecastDay.temp.max
+        )}</span> /
+        <span class="weather-forecast-temperature-min">${Math.round(
+          forecastDay.temp.min
+        )}</span>
       </div>
     </div>`;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
 
@@ -52,7 +66,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   apiKey = "0ec90f7c009a99f423602e64344f4416";
   apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
   console.log(apiUrl);
